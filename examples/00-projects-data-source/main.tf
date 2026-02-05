5ac76db9d47fa4e9c4d398fe5d5b5c73f701a837
Eugene Rubanov <eugene.rubanov@neo4j.com>
2026-02-05 14:55:50 +0000

    chore: update examples formatting

--- examples/00-projects-data-source/main.tf
+++ examples/00-projects-data-source/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.13.4"
   required_providers {
     neo4jaura = {
-      source = "neo4j-labs/neo4jaura"
+      source  = "neo4j-labs/neo4jaura"
       version = "0.0.2-beta"
     }
   }
